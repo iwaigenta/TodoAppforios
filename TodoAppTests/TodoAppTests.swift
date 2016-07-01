b36e640400a2fa5e9b5d1f12886b158ebfef104c
GENTA <PC149@1st-iwai-no-macbook-air.local>
2016-07-01 18:59:41 +0900

    Initial Commit

---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by 1st_iwai on 2016/07/01.
+//  Copyright © 2016年 1st_iwai. All rights reserved.
+//
+
+import XCTest
+@testable import TodoApp
+
+class TodoAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
